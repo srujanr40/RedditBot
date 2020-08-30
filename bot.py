5257d047abace635e905ec9600492d0a432206e4
Srujan <srujanr40@gmail.com>
2020-08-30 16:36:50 -0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,44 @@
+import praw
+import re
+import os
+import pdb
+
+
+reddit = praw.Reddit('bot')
+
+if not os.path.isfile("saved_posts.txt"):
+    saved_posts = []
+
+else:
+    with open("saved_posts.txt", "r") as file:
+        saved_posts = file.read()
+        saved_posts = saved_posts.split("\n")
+        saved_posts = list(filter(None, saved_posts))   #For getting rid of all empty post_ids in the list 
+                                                        #From https://stackoverflow.com/questions/40598248/removing-all-empty-elements-from-a-list
+
+sub = input("Enter the name of the subreddit (without the 'r/'): ")
+subreddit = reddit.subreddit(sub)
+
+searchText = input("Enter the keywords to search for: ")
+
+for submission in subreddit.new(limit=20):
+
+    if submission.id not in saved_posts:
+        
+        if re.search(searchText, submission.title, re.IGNORECASE):
+            print (submission.title)
+            print (submission.selftext)
+            print (submission.url)
+            print ("---------------------------------------------------------------------------------------------------------------------")
+            #From https://github.com/praw-dev/praw/blob/master/docs/getting_started/quick_start.rst
+
+            submission.save()
+            saved_posts.append(submission.id)
+
+
+with open("saved_posts", "w") as file:
+    for post_id in saved_posts:
+        file.write(post_id + "\n")
+
+
+
